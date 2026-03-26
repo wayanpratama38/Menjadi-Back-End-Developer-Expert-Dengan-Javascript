031b566b65ed35529475bdce58d82b28edde9d15
wayanpratama38 <wayanpratama38@gmail.com>
2026-03-27 02:43:31 +0700

    feat: :white_check_mark: finish add functionality TDD

--- AutomationTesting/MathAPI/src/MathBasic.js
+++ AutomationTesting/MathAPI/src/MathBasic.js
@@ -1,6 +1,16 @@
 const MathBasic = {
-    add : () => {
+    add : (...args) => {
+        if(args.length !== 2) {
+            throw new Error('Fungsi add hanya menerima dua parameter')
+        }
 
+        const [a, b] = args;
+ 
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number');
+        }
+
+        return a + b;
     },
 
     subtract : () => {
--- AutomationTesting/MathAPI/src/MathBasic.test.js
+++ AutomationTesting/MathAPI/src/MathBasic.test.js
@@ -13,3 +13,27 @@ describe('A MathBasic', ()=> {
         expect(MathBasic.divide).toBeInstanceOf(Function);
     })
 })
+
+describe('A add function',() => {
+    it('should throw error when not given 2 paramaters', () => {
+        expect(()=> MathBasic.add()).toThrow();
+        expect(()=> MathBasic.add(1)).toThrow();
+        expect(()=> MathBasic.add(1,2,3)).toThrow();
+        expect(()=> MathBasic.add(1,2,3,4)).toThrow();
+
+
+    });
+
+    it('should throw error when given non-number paramaters', () => {
+        expect(()=> MathBasic.add('1','2')).toThrow();
+        expect(()=> MathBasic.add(true,{})).toThrow();
+        expect(()=> MathBasic.add(null,false)).toThrow();
+
+    })
+
+    it('should return a + b when given two number parameters', () => {
+      expect(MathBasic.add(2, 2)).toEqual(4);
+      expect(MathBasic.add(16, 8)).toEqual(24);
+      expect(MathBasic.add(3, 7)).toEqual(10);
+    });
+})
