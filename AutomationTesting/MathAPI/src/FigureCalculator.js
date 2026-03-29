c42bd893f2acfe8688b12449b2b1b011a21562e7
wayanpratama38 <wayanpratama38@gmail.com>
2026-03-29 23:31:28 +0700

    feat: create a fully unit test and functionality for the FigureCalculator

--- AutomationTesting/MathAPI/src/FigureCalculator.js
+++ AutomationTesting/MathAPI/src/FigureCalculator.js
@@ -18,7 +18,46 @@ export default class FigureCalculator{
         return this._mathBasic.multiply(2,this._mathBasic.add(a,b));
 
     }
-    calculateRectangleArea() { }
-    calculateTrianglePerimeter() { }
-    calculateTriangleArea() { }
+    calculateRectangleArea(...args) { 
+        if(args.length!==2) {
+            throw new Error("Memerlukan dua parameter")
+        }
+
+        const [a,b] = args;
+
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number');
+        }
+
+        return this._mathBasic.multiply(a,b);
+    }
+
+    calculateTrianglePerimeter(...args) {
+        if(args.length!==3) {
+            throw new Error("Memerlukan tiga parameter")
+        }
+
+        const [a,b,c] = args;
+
+        if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number');
+        
+        }
+
+        return this._mathBasic.add(this._mathBasic.add(a,b),c);
+    }
+
+    calculateTriangleArea(...args) {
+        if(args.length!==2) {
+            throw new Error("Memerlukan dua parameter")
+        }
+
+        const [a,b] = args;
+
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number');
+        }
+
+        return this._mathBasic.multiply(0.5,this._mathBasic.add(a,b));
+     }
 }
--- AutomationTesting/MathAPI/src/FigureCalculator.test.js
+++ AutomationTesting/MathAPI/src/FigureCalculator.test.js
@@ -48,13 +48,126 @@ describe('calculateRectanglePerimeter method', ()=> {
         const spyMultiply = vi.spyOn(MathBasic, "multiply");
         const figureCalculator = new FigureCalculator(MathBasic);
 
-        // action
+        // Action
         const result = figureCalculator.calculateRectanglePerimeter(length,width);
 
+        // Assert
         expect(result).toEqual(60);
         expect(spyAdd).toHaveBeenCalledWith(length,width);
         expect(spyMultiply).toHaveBeenCalledWith(2,30);
     })
+})
+
+describe('calculateRectangleArea method', ()=> {
+    it('should throw error when not given 2 paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
 
 
+        expect(()=> figureCalculator.calculateRectangleArea()).toThrow();
+        expect(()=> figureCalculator.calculateRectangleArea(1)).toThrow();
+        expect(()=> figureCalculator.calculateRectangleArea(1,2,3)).toThrow();
+        expect(()=> figureCalculator.calculateRectangleArea(1,2,3,4)).toThrow();
+
+
+    });
+
+    it('should throw error when given non-number paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
+
+        expect(()=> figureCalculator.calculateRectangleArea('1','2')).toThrow();
+        expect(()=> figureCalculator.calculateRectangleArea(true,{})).toThrow();
+        expect(()=> figureCalculator.calculateRectangleArea(null,false)).toThrow();
+    })
+
+    it('should return correct value based on rectangle area formula',()=>{
+        // Arrange
+        const length = 5;
+        const width = 2;
+        const spyMultiply = vi.spyOn(MathBasic, "multiply");
+        const figureCalculator = new FigureCalculator(MathBasic);
+
+        // Action
+        const result = figureCalculator.calculateRectangleArea(length,width);
+
+        // Assert
+        expect(result).toEqual(10);
+        expect(spyMultiply).toHaveBeenCalledWith(length,width);
+    })
+})
+
+describe('calculateTrianglePerimeter method', ()=> {
+    it('should throw error when not given 3 paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
+
+
+        expect(()=> figureCalculator.calculateTrianglePerimeter()).toThrow();
+        expect(()=> figureCalculator.calculateTrianglePerimeter(1)).toThrow();
+        expect(()=> figureCalculator.calculateTrianglePerimeter(1,2)).toThrow();
+        expect(()=> figureCalculator.calculateTrianglePerimeter(1,2,3,4)).toThrow();
+    });
+
+    it('should throw error when given non-number paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
+
+        expect(()=> figureCalculator.calculateTrianglePerimeter('1','2','2')).toThrow();
+        expect(()=> figureCalculator.calculateTrianglePerimeter(true,{},false)).toThrow();
+        expect(()=> figureCalculator.calculateTrianglePerimeter(null,false,{})).toThrow();
+    })
+
+    it('should return correct value based on rectangle area formula',()=>{
+        // Arrange
+        const a = 5;
+        const b = 2;
+        const c = 3;
+        const spyAdd = vi.spyOn(MathBasic, "add")
+        const figureCalculator = new FigureCalculator(MathBasic);
+
+        // Action
+        const result = figureCalculator.calculateTrianglePerimeter(a,b,c);
+
+        // Assert
+        expect(result).toEqual(10);
+        expect(spyAdd).toHaveBeenCalledWith(a,b);
+        expect(spyAdd).toHaveBeenCalledWith(7,c)
+    })
+})
+
+
+describe('calculateTriangleArea method', ()=> {
+    it('should throw error when not given 2 paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
+
+
+        expect(()=> figureCalculator.calculateTriangleArea()).toThrow();
+        expect(()=> figureCalculator.calculateTriangleArea(1)).toThrow();
+        expect(()=> figureCalculator.calculateTriangleArea(1,2,3)).toThrow();
+        expect(()=> figureCalculator.calculateTriangleArea(1,2,3,4)).toThrow();
+
+
+    });
+
+    it('should throw error when given non-number paramaters', () => {
+        const figureCalculator = new FigureCalculator({});
+
+        expect(()=> figureCalculator.calculateTriangleArea('1','2')).toThrow();
+        expect(()=> figureCalculator.calculateTriangleArea(true,{})).toThrow();
+        expect(()=> figureCalculator.calculateTriangleArea(null,false)).toThrow();
+    })
+
+    it('should return correct value based on rectangle area formula',()=>{
+        // Arrange
+        const length = 5;
+        const width = 2;
+        const spyAdd = vi.spyOn(MathBasic, "add")
+        const spyMultiply = vi.spyOn(MathBasic, "multiply");
+        const figureCalculator = new FigureCalculator(MathBasic);
+
+        // Action
+        const result = figureCalculator.calculateTriangleArea(length,width);
+
+        // Assert
+        expect(result).toEqual(3.5);
+        expect(spyAdd).toHaveBeenCalledWith(length,width)
+        expect(spyMultiply).toHaveBeenCalledWith(0.5,7);
+    })
 })
