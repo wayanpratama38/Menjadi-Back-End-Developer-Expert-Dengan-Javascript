66b645022ee8d40a7d96f1f1f58bb219cd044721
wayanpratama38 <wayanpratama38@gmail.com>
2026-04-15 16:12:14 +0700

    feat: adding a new domain error translation

--- FirstSubmission-ForumAPI/src/Commons/exceptions/DomainErrorTranslator.js
+++ FirstSubmission-ForumAPI/src/Commons/exceptions/DomainErrorTranslator.js
@@ -22,7 +22,8 @@ DomainErrorTranslator._directories = {
   'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION' : new InvariantError('tidak dapat membuat thread baru karena tipe data tidak sesuai'),
   'NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY' : new InvariantError('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada'),
   'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION' : new InvariantError('idak dapat membuat thread baru karena tipe data tidak sesuai'),
-  'THREAD_REPOSITORY_POSTGRES.NOT_FOUND' : new NotFoundError('thread tidak dapat ditemukan')
+  'THREAD_REPOSITORY_POSTGRES.NOT_FOUND' : new NotFoundError('thread tidak dapat ditemukan'),
+  'COMMENT_REPOSITORY_POSTGRES.NOT_FOUND' : new NotFoundError('komen tidak ditemukan'),
 };
 
 export default DomainErrorTranslator;
